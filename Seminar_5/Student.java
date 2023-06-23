23ece2e265412b33a574b6959cd8433ebd38f83a
Grigory-Chentsov <121394273+Grigory-Chentsov@users.noreply.github.com>
2023-06-23 18:17:56 +0300

    Create Student.java

---
+++ Seminar_5/Student.java
@@ -0,0 +1,12 @@
+class Student {
+    private int id;
+    // other student fields
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
